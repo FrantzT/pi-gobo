b5aaea2b91e5c2d314074d082ac6ed7e9e482a3e
FrantzT <pawel.a.zajac@gmail.com>
2020-03-31 11:31:00 -0400

    Update DMX_TX_v1_working.ino

--- DMX_TX_v1_working.ino
+++ DMX_TX_v1_working.ino
@@ -1,28 +1,24 @@
-/* Welcome to DmxSimple. This library allows you to control DMX stage and
-** architectural lighting and visual effects easily from Arduino. DmxSimple
-** is compatible with the Tinker.it! DMX shield and all known DIY Arduino
-** DMX control circuits.
-**
-** DmxSimple is available from: http://code.google.com/p/tinkerit/
-** Help and support: http://groups.google.com/group/dmxsimple       */
-
-/* To use DmxSimple, you will need the following line. Arduino will
-** auto-insert it if you select Sketch > Import Library > DmxSimple. */
+/* 
+This is basic transmitter, whcih in the project is used for test reason.
+It will otput DMX formated signal (but not actuall electrical DMX for this you would need to setup appropiate converter).
+The DMXSimple library is required to be installed into you Arudion library, 
+it is availbe for download here including additional documentation.
+https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/tinkerit/DmxSimple_v3.zip */
 
 #include <DmxSimple.h>
 
 void setup() {
+  
+  /* Standard transmition rate for DMX protocol */
+  
   Serial.begin(250000);
   
-  /* The most common pin for DMX output is pin 3, which DmxSimple
-  ** uses by default. If you need to change that, do it here. */
+  /* Here we are setting up the output pin number */
+  
   DmxSimple.usePin(3);
 
-  /* DMX devices typically need to receive a complete set of channels
-  ** even if you only need to adjust the first channel. You can
-  ** easily change the number of channels sent here. If you don't
-  ** do this, DmxSimple will set the maximum channel number to the
-  ** highest channel you DmxSimple.write() to. */
+  /* Here we are setting up 3 output channels for for R,G,B values for our Pi reciever */
+  
   DmxSimple.maxChannel(3);
   
 }
@@ -37,14 +33,11 @@ void loop() {
           DmxSimple.write(1,i);
           DmxSimple.write(2,j);
           DmxSimple.write(3,k);
-          delay(1);
+          delay(1); /* Small delay to slow down the ramping, make more stabile reception on reciever */
         }
       }
     }
-    /* Small delay to slow down the ramping */
     
-
-    Serial.println();
-  //}
+    Serial.println(); // printing values to serial monitor just for test
 
 }
